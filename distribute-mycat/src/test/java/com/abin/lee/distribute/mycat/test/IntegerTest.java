f20d415c0dd2184fcc7c5cc7feea687dbeaa7d6b
zondahuman <zondahuman@gmail.com>
2017-05-11 23:23:22 +0800

    add

---
+++ distribute-mycat/src/test/java/com/abin/lee/distribute/mycat/test/IntegerTest.java
@@ -0,0 +1,14 @@
+package com.abin.lee.distribute.mycat.test;
+
+/**
+ * Created by abin on 2017/5/11 2017/5/11.
+ * distribute-svr
+ * com.abin.lee.distribute.mycat.test
+ */
+public class IntegerTest {
+
+    public static void main(String[] args) {
+        String result = Integer.toString(10);
+        System.out.println(result);
+    }
+}
