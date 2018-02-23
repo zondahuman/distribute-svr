4140ae8ed56a9bbad53852a56cb9c51963b77238
zondahouse <zondahouse@gmail.com>
2018-02-23 17:40:35 +0800

    add

--- distribute-mycat/src/test/java/com/abin/lee/distribute/mycat/test/IntegerTest.java
+++ distribute-mycat/src/test/java/com/abin/lee/distribute/mycat/test/IntegerTest.java
@@ -1,5 +1,10 @@
 package com.abin.lee.distribute.mycat.test;
 
+import com.google.common.collect.Lists;
+import org.junit.Test;
+
+import java.util.List;
+
 /**
  * Created by abin on 2017/5/11 2017/5/11.
  * distribute-svr
@@ -13,4 +18,27 @@ public class IntegerTest {
         System.out.println("1111");
 
     }
+
+
+    @Test
+    public void test(){
+        List<Integer> list = Lists.newArrayList();
+        for (int i = 0; i <5 ; i++) {
+            list.add(i);
+        }
+        int baseTwo = 2;
+        for (int temp : list) {
+            Integer result = temp % baseTwo;
+            System.out.println("baseTwo-----temp=" + temp + " ,result="+result);
+        }
+        System.out.println("--------------------------------------------------------");
+        int baseFour = 4;
+        for (int temp : list) {
+            Integer result = temp % baseFour;
+            System.out.println("baseFour-----temp==" + temp + " ,result="+result);
+        }
+    }
+
+
+
 }
